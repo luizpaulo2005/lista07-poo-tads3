50ee7efaab3bc5ad929fde2234e8cc25086d9da6
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2024-03-25 22:51:08 -0300

    fix: ex02.1

--- ex02/1.js
+++ ex02/1.js
@@ -18,16 +18,18 @@ class Animal {
 
 class Mamifero extends Animal {
   #tipoDePelo;
+  #som;
 
-  constructor(nome, idade, tipoDePelo) {
+  constructor(nome, idade, tipoDePelo, som) {
     super(nome, idade);
     this.#tipoDePelo = tipoDePelo;
+    this.#som = som;
   }
 
   #informacoes() {
-    return `${super.retornaInformacoes} e seu tipo de pelo é ${
+    return `${super.retornaInformacoes}, seu tipo de pelo é ${
       this.#tipoDePelo
-    }`;
+    } e faz esse som: ${this.#som}`;
   }
 
   get retornaInformacoes() {
@@ -37,24 +39,25 @@ class Mamifero extends Animal {
 
 class Ave extends Animal {
   #tipoDeBico;
-
-  constructor(nome, idade, tipoDeBico) {
+  #podeVoar;
+  constructor(nome, idade, tipoDeBico, podeVoar) {
     super(nome, idade);
     this.#tipoDeBico = tipoDeBico;
+    this.#podeVoar = podeVoar;
   }
 
   #informacoes() {
-    return `${super.retornaInformacoes} e seu tipo de bico é ${
+    return `${super.retornaInformacoes}, seu tipo de bico é ${
       this.#tipoDeBico
-    }`;
+    } e ele ${!this.#podeVoar ? "não pode" : "pode"} voar`;
   }
 
   get retornaInformacoes() {
     return this.#informacoes();
   }
 }
+const macacego = new Mamifero("Morcego", 4, "Morc", "Grita");
+console.log(macacego.retornaInformacoes);
 
-const tucano = new Ave("Tucano", 2, "Curvo");
-console.log(tucano.retornaInformacoes)
-const macacego = new Mamifero("Morcego", 4, "Morc");
-console.log(macacego.retornaInformacoes)
+const tucano = new Ave("Tucano", 2, "Curvo", false);
+console.log(tucano.retornaInformacoes);
